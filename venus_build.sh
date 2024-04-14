825b6a8ffa9bc8fc700f9159cfcd1079259ea6ac
sofiworker <sofiworker@outlook.com>
2024-04-14 20:28:56 +0800

    fix:add venus static build shell and CA expiration time is 10 years

---
+++ venus_build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+make all GOLDFLAGS='-extldflags "-static"'
+
+make all WHAT=cmd/kube-apiserver GOFLAGS=-v GOLDFLAGS='-extldflags "-static"'
+make all WHAT=cmd/kube-controller-manager GOFLAGS=-v GOLDFLAGS='-extldflags "-static"'
+make all WHAT=cmd/kube-proxy GOFLAGS=-v GOLDFLAGS='-extldflags "-static"'
+make all WHAT=cmd/kubeadm GOFLAGS=-v GOLDFLAGS='-extldflags "-static"'
+make all WHAT=cmd/kubectl GOFLAGS=-v GOLDFLAGS='-extldflags "-static"'
+make all WHAT=cmd/kubectl-convert GOFLAGS=-v GOLDFLAGS='-extldflags "-static"'
+make all WHAT=cmd/kubelet GOFLAGS=-v GOLDFLAGS='-extldflags "-static"'
+make all WHAT=cmd/kubemark GOFLAGS=-v GOLDFLAGS='-extldflags "-static"'
